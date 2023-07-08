6611c62901c68d413e39091c4ad44ccf8bcf5451
Ayush <ayush@Ayushs-MacBook-Air.local>
2023-07-08 22:26:45 +0530

    navigation added

--- pages/index.js
+++ pages/index.js
@@ -1,214 +1,42 @@
-import { useState, useRef, useEffect } from "react";
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Image from "next/image";
-import ReactMarkdown from "react-markdown";
-import CircularProgress from "@mui/material/CircularProgress";
+import React, { useState } from "react";
+import axios from "axios";
 import { useRouter } from "next/router";
 
-export default function Home() {
-  const [userInput, setUserInput] = useState("");
-  const [history, setHistory] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const {
-    query: { fileName },
-  } = useRouter();
-  const [messages, setMessages] = useState([
-    {
-      message: "Hi there! How can I help?",
-      type: "apiMessage",
-    },
-  ]);
+const FileUploader = () => {
+  const [file, setFile] = useState(null);
+  const router = useRouter();
 
-  const messageListRef = useRef(null);
-  const textAreaRef = useRef(null);
-
-  // Auto scroll chat to bottom
-  useEffect(() => {
-    const messageList = messageListRef.current;
-    messageList.scrollTop = messageList.scrollHeight;
-  }, [messages]);
-
-  // Focus on text field on load
-  useEffect(() => {
-    textAreaRef.current.focus();
-  }, []);
-
-  // Handle errors
-  const handleError = () => {
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      {
-        message: "Oops! There seems to be an error. Please try again.",
-        type: "apiMessage",
-      },
-    ]);
-    setLoading(false);
-    setUserInput("");
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
   };
 
-  // Handle form submission
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    if (userInput.trim() === "") {
-      return;
-    }
+    const formData = new FormData();
+    formData.append("file", file);
 
-    setLoading(true);
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { message: userInput, type: "userMessage" },
-    ]);
-
-    // Send user question and history to API
-    const response = await fetch("/api/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ question: userInput, history: history, fileName }),
-    });
-
-    if (!response.ok) {
-      handleError();
-      return;
-    }
-
-    // Reset user input
-    setUserInput("");
-    const data = await response.json();
-    if (data.error) {
-      handleError();
-      return;
-    }
-
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { message: data.message, type: "apiMessage" },
-    ]);
-    setLoading(false);
-  };
-
-  // Prevent blank submissions and allow for multiline input
-  const handleEnter = (e) => {
-    if (e.key === "Enter" && userInput) {
-      if (!e.shiftKey && userInput) {
-        handleSubmit(e);
-      }
-    } else if (e.key === "Enter") {
-      e.preventDefault();
+    try {
+      const response = await axios.post("/api/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      router.push({
+        pathname: "/chat",
+        query: { fileName: response.data?.fileName },
+      });
+    } catch (error) {
+      console.error("Error:", error);
     }
   };
-
-  // Keep history in sync with messages
-  useEffect(() => {
-    if (messages.length >= 3) {
-      setHistory([
-        [
-          messages[messages.length - 2].message,
-          messages[messages.length - 1].message,
-        ],
-      ]);
-    }
-  }, [messages]);
 
   return (
-    <>
-      <main className={styles.main}>
-        <div className={styles.cloud}>
-          <div ref={messageListRef} className={styles.messagelist}>
-            {messages.map((message, index) => {
-              return (
-                // The latest message sent by the user will be animated while waiting for a response
-                <div
-                  key={index}
-                  className={
-                    message.type === "userMessage" &&
-                    loading &&
-                    index === messages.length - 1
-                      ? styles.usermessagewaiting
-                      : message.type === "apiMessage"
-                      ? styles.apimessage
-                      : styles.usermessage
-                  }
-                >
-                  {/* Display the correct icon depending on the message type */}
-                  {message.type === "apiMessage" ? (
-                    <Image
-                      src="/parroticon.png"
-                      alt="AI"
-                      width="30"
-                      height="30"
-                      className={styles.boticon}
-                      priority={true}
-                    />
-                  ) : (
-                    <Image
-                      src="/usericon.png"
-                      alt="Me"
-                      width="30"
-                      height="30"
-                      className={styles.usericon}
-                      priority={true}
-                    />
-                  )}
-                  <div className={styles.markdownanswer}>
-                    {/* Messages are being rendered in Markdown format */}
-                    <ReactMarkdown linkTarget={"_blank"}>
-                      {message.message}
-                    </ReactMarkdown>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <div className={styles.center}>
-          <div className={styles.cloudform}>
-            <form onSubmit={handleSubmit}>
-              <textarea
-                disabled={loading}
-                onKeyDown={handleEnter}
-                ref={textAreaRef}
-                autoFocus={false}
-                rows={1}
-                maxLength={512}
-                type="text"
-                id="userInput"
-                name="userInput"
-                placeholder={
-                  loading ? "Waiting for response..." : "Type your question..."
-                }
-                value={userInput}
-                onChange={(e) => setUserInput(e.target.value)}
-                className={styles.textarea}
-              />
-              <button
-                type="submit"
-                disabled={loading}
-                className={styles.generatebutton}
-              >
-                {loading ? (
-                  <div className={styles.loadingwheel}>
-                    <CircularProgress color="inherit" size={20} />{" "}
-                  </div>
-                ) : (
-                  // Send icon SVG in input field
-                  <svg
-                    viewBox="0 0 20 20"
-                    className={styles.svgicon}
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-                  </svg>
-                )}
-              </button>
-            </form>
-          </div>
-          <div className={styles.footer}></div>
-        </div>
-      </main>
-    </>
+    <form onSubmit={handleSubmit}>
+      <input type="file" accept="application/pdf" onChange={handleFileChange} />
+      <button type="submit">Upload</button>
+    </form>
   );
-}
+};
+
+export default FileUploader;
