289553680d0c6112b394eb0d9641eaa5d985b9a1
divya <dpj221@gmail.com>
2023-08-24 20:24:49 +0530

    updated file upload wrapper

--- components/home/FromUrl.tsx
+++ components/home/FromUrl.tsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { Button } from '@chakra-ui/react'
 import isUrl from "is-url";
 import axios from "axios";
-import { useAuth } from "../../store/useAuth"
+import { useAuth } from "../../store/useAuth";
+import { FileUploadWrapper } from "./FileUploadWrapper";
 
 export const FromUrl = () => {
   const [error, setError] = useState(false);
@@ -27,28 +28,31 @@ export const FromUrl = () => {
 
   return (
     <>
-      <form onSubmit={saveAsPDF}>
-        <div className="field has-addons">
-          <div className="control is-expanded">
-            <input
-              className={`input is-fullwidth ${error ? "is-danger" : ""}`}
-              type="text"
-              name="url"
-              placeholder="Enter PDF URL. Ex: https://url.com/path/file.pdf"
-            />
-            {error && <p className="has-text-danger">Invalid URL</p>}
+      <FileUploadWrapper>
+        <form onSubmit={saveAsPDF}>
+          <div className="field has-addons">
+            <div className="control is-expanded">
+              <input
+                className={`input is-fullwidth ${error ? "is-danger" : ""}`}
+                type="text"
+                name="url"
+                placeholder="Enter PDF URL. Ex: https://url.com/path/file.pdf"
+              />
+              {error && <p className="has-text-danger">Invalid URL</p>}
+            </div>
+            <div className="control">
+              <Button
+                type="submit"
+                border='2px'
+                borderColor='black'
+                variant='outline'>
+                Upload
+              </Button>
+            </div>
           </div>
-          <div className="control">
-            <Button
-              type="submit"
-              border='2px'
-              borderColor='black'
-              variant='outline'>
-              Upload
-            </Button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </FileUploadWrapper>
+
     </>
   );
 };
