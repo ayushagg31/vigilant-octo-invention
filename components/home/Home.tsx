56e8cd832b7dc8da40f2007ff2e14fedb5576569
divya <dpj221@gmail.com>
2023-08-26 02:25:14 +0530

    added tags for current pdfs

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,15 +1,19 @@
+'use client'
 import React, { useEffect, useState } from "react";
-import { Container } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { Container, Box, HStack, Tag, TagLabel, TagRightIcon, Wrap, WrapItem, Text, TagCloseButton, Center } from '@chakra-ui/react'
 import { useDashboard } from "../../store/useDashboard";
 import { useCollections } from "../../store/useCollections"
 import { BeforeUpload } from "./BeforeUpload";
 import { useRouter } from 'next/router';
-import { useAuth } from "../../store/useAuth"
+import { useAuth } from "../../store/useAuth";
 import { fetchCollectionsApi } from "../../services/client.utils";
+import { Link } from '@chakra-ui/react'
+import { AiOutlineLink } from 'react-icons/ai';
 import axios from "axios"
-
 const Home = () => {
-  const router = useRouter()
+  const router = useRouter();
+  const [mounted, setMounted] = useState(false);
   const { showResult, collectionId, } = useDashboard((store) => {
     return {
       showResult: store.showResult,
@@ -43,21 +47,51 @@ const Home = () => {
     }
     fetchCollections()
   }, [user])
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
-
+  if (!mounted) return <></>;
   return (
     <div style={{ color: "#000" }}>
       <Container>
         <div>
           <BeforeUpload />
         </div>
-        {/* <div style={{ display: "flex" }}>
-          {collections.map(({ collectionId, collectionName }) =>
-            <div style={{ width: "300px", height: "100px", textAlign: "center", margin: "auto", border: "1px solid black" }}>
-              <a href={`/docinsights?id=${collectionId}`} >{collectionName}</a>
-            </div>
-          )}
-        </div> */}
+        <Box p={4}>
+          <Center>
+            <Text as='b' fontSize='xl'>
+              Your Documents
+            </Text>
+          </Center>
+
+        </Box>
+        <Box>
+          <div style={{ display: "flex" }}>
+            <Wrap border='1px'
+              borderColor='rgb(226, 232, 240)'
+              borderRadius='5px'
+              p={5}
+            >
+              {collections.map(({ collectionId, collectionName }) =>
+
+                <WrapItem>
+                  <Tag size='sm' key={collectionId} border='2px' borderColor={'black'} colorScheme='gray' variant="subtle">
+                    <TagLabel>
+                      <Link as={NextLink} href={`/docinsights?id=${collectionId}`}>
+                        {collectionName}
+                      </Link>
+                    </TagLabel>
+                    <AiOutlineLink />
+                    <TagCloseButton />
+                  </Tag>
+                </WrapItem>
+
+              )}
+            </Wrap>
+          </div>
+
+        </Box>
       </Container >
     </div >
   );
