26066654e98834a46f757f0c5f9b4b147f84149f
CV Vikash <cvvkshcv@gmail.com>
2023-09-14 18:59:37 +0530

    Zero tire

--- services/firestore.service.js
+++ services/firestore.service.js
@@ -239,7 +239,7 @@ export const fetchPlanInfo = async ({ userEmail }) => {
     const userDoc = await getDoc(userRef);
     let planInfo;
     if (userDoc.exists()) {
-      planInfo = (await userDoc.data()["currentPlan"]) || "plus_tier";
+      planInfo = (await userDoc.data()["currentPlan"]) || "free_tier";
     }
     return planInfo;
   } catch (e) {
