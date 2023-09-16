0f7c25da933ec7091da6aceab8633af38c68478b
CV Vikash <cvvkshcv@gmail.com>
2023-09-16 14:09:49 +0530

    Fixed nav and sliding section

--- components/landing/Features.tsx
+++ components/landing/Features.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   Container,
@@ -11,67 +11,76 @@ import {
   StackDivider,
   Icon,
   useColorModeValue,
-} from '@chakra-ui/react'
-import { ReactElement } from 'react';
-import { FiUploadCloud, } from 'react-icons/fi'
-import { BsFillChatLeftTextFill, } from 'react-icons/bs'
+} from "@chakra-ui/react";
+import { ReactElement } from "react";
+import { FiUploadCloud } from "react-icons/fi";
+import { BsFillChatLeftTextFill } from "react-icons/bs";
 
 interface FeatureProps {
-  text: string
-  iconBg: string
-  icon?: ReactElement
+  text: string;
+  iconBg: string;
+  icon?: ReactElement;
 }
 
 const Feature = ({ text, icon, iconBg }: FeatureProps) => {
   return (
-    <Stack direction={'row'} align={'center'}>
-      <Flex w={8} h={8} align={'center'} justify={'center'} rounded={'full'} bg={iconBg}>
+    <Stack direction={"row"} align={"center"}>
+      <Flex
+        w={8}
+        h={8}
+        align={"center"}
+        justify={"center"}
+        rounded={"full"}
+        bg={iconBg}
+      >
         {icon}
       </Flex>
       <Text fontWeight={600}>{text}</Text>
     </Stack>
-  )
-}
+  );
+};
 
 export default function SplitWithImage() {
   return (
-    <div id="how-it-works">
-      <Container maxW={'5xl'} py={12}>
-        <div className='demo-video'>
+    <div id="demo-section">
+      <Container maxW={"5xl"} py={12}>
+        <div className="demo-video">
           <Stack spacing={2}>
-            <Text as='b' fontSize={'xl'}>
+            <Text as="b" fontSize={"xl"}>
               Get insights in 2 simple steps
             </Text>
             <Stack
               spacing={4}
               divider={
-                <StackDivider borderColor={useColorModeValue('gray.100', 'gray.700')} />
-              }>
+                <StackDivider
+                  borderColor={useColorModeValue("gray.100", "gray.700")}
+                />
+              }
+            >
               <Feature
                 icon={<Icon as={FiUploadCloud} w={5} h={5} />}
-                iconBg={useColorModeValue('yellow.100', 'yellow.900')}
-                text={'Upload or link your PDFs and videos.'}
+                iconBg={useColorModeValue("yellow.100", "yellow.900")}
+                text={"Upload or link your PDFs and videos."}
               />
               <Feature
                 icon={<Icon as={BsFillChatLeftTextFill} w={5} h={5} />}
-                iconBg={useColorModeValue('green.100', 'green.900')}
-                text={'Chat with our AI-powered chatbot.'}
+                iconBg={useColorModeValue("green.100", "green.900")}
+                text={"Chat with our AI-powered chatbot."}
               />
             </Stack>
           </Stack>
           <Flex>
             <Image
-              boxShadow={'xl'}
-              border={'1px'}
-              rounded={'md'}
-              alt={'feature image'}
-              src={'/sitegpt.gif'}
-              objectFit={'scale-down'}
+              boxShadow={"xl"}
+              border={"1px"}
+              rounded={"md"}
+              alt={"feature image"}
+              src={"/sitegpt.gif"}
+              objectFit={"scale-down"}
             />
           </Flex>
         </div>
       </Container>
     </div>
-
-  )
+  );
 }
--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   Flex,
@@ -11,22 +11,21 @@ import {
   IconProps,
   useDisclosure,
   Highlight,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 import { useAuth } from "../../store/useAuth";
 import { useRouter } from "next/router";
 import { IoMdArrowForward } from "react-icons/io";
 
-
 import { LoginModal } from "../home/LoginModal";
 
 export default function Hero() {
-  let router = useRouter()
+  let router = useRouter();
 
   const {
     isOpen: isOpenLoginModal,
     onOpen: onOpenLoginModal,
-    onClose: onCloseLoginModal
-  } = useDisclosure()
+    onClose: onCloseLoginModal,
+  } = useDisclosure();
 
   const { user, loadingUser, logout } = useAuth((store) => ({
     user: store.user,
@@ -34,78 +33,97 @@ export default function Hero() {
     logout: store.logout,
   }));
 
-
-  const gotoDashboard = () => router.push('/dashboard');
+  const gotoDashboard = () => router.push("/dashboard");
   const tryItHandler = () => {
     if (user) {
-      gotoDashboard()
+      gotoDashboard();
     } else {
-      onOpenLoginModal()
+      onOpenLoginModal();
     }
-  }
+  };
 
   const demoHandler = () => {
-    const url = '/docinsights?id=f5fa43d2-4497-49f4-afd4-8770af911c6f';
-    router.push(url)
-  }
-
-
-  // method to jump to the desired element by using the element's id
-  const jumpToReleventDiv = (id) => {
-    const releventDiv = document.getElementById(id);
-    // behavior: "smooth" parameter for smooth movement
-    releventDiv.scrollIntoView({ behavior: "smooth", block: 'center' });
-  }
+    const url = "/docinsights?id=f5fa43d2-4497-49f4-afd4-8770af911c6f";
+    router.push(url);
+  };
 
   return (
     <>
-      <Container maxW={'5xl'}>
+      <Container maxW={"5xl"} id="how-it-works">
         <Stack
-          textAlign={'center'}
-          align={'center'}
+          textAlign={"center"}
+          align={"center"}
           spacing={{ base: 4, md: 5 }}
-          py={{ base: 20, md: 28 }}>
+          py={{ base: 20, md: 28 }}
+        >
           <div>
             <Heading
               fontWeight={800}
-              className='hero-heading'
-              color={'#343a40'}
-              fontSize={{ base: '3xl', sm: '4xl', md: '5xl' }}
-              lineHeight={'110%'}>
-              <Highlight query='Save countless hours'
-                styles={{ px: '1', py: '1', bg: 'black', color: '#fff', fontFamily: 'Inconsolata' }}>
+              className="hero-heading"
+              color={"#343a40"}
+              fontSize={{ base: "3xl", sm: "4xl", md: "5xl" }}
+              lineHeight={"110%"}
+            >
+              <Highlight
+                query="Save countless hours"
+                styles={{
+                  px: "1",
+                  py: "1",
+                  bg: "black",
+                  color: "#fff",
+                  fontFamily: "Inconsolata",
+                }}
+              >
                 Chat With Documents & Youtube Videos Save countless hours
               </Highlight>
             </Heading>
           </div>
-          <Text as='b' className='hero-heading' fontWeight={600} fontSize={{ base: 'md', sm: 'lg' }} maxW={'4xl'}>
-            Summarize your content, Ask questions and Create notes. Almost Instantly.
+          <Text
+            as="b"
+            className="hero-heading"
+            fontWeight={600}
+            fontSize={{ base: "md", sm: "lg" }}
+            maxW={"4xl"}
+          >
+            Summarize your content, Ask questions and Create notes. Almost
+            Instantly.
           </Text>
-          <Stack spacing={6} direction={'row'}>
+          <Stack spacing={6} direction={"row"}>
             <Button
               rightIcon={<IoMdArrowForward />}
               onClick={() => {
                 tryItHandler();
               }}
-              rounded={'full'}
+              rounded={"full"}
               px={6}
-              colorScheme={'gray'}
+              colorScheme={"gray"}
               bg="#343a40"
-              _hover={{ bg: '#23272b', color: '#fff', borderColor: '#1d2124', borderWidth: '1px' }}
-              color="#fff">
+              _hover={{
+                bg: "#23272b",
+                color: "#fff",
+                borderColor: "#1d2124",
+                borderWidth: "1px",
+              }}
+              color="#fff"
+            >
               Start for free
             </Button>
 
-            <Button onClick={() => demoHandler()} rounded={'full'} px={6}>See it in action
+            <Button onClick={() => demoHandler()} rounded={"full"} px={6}>
+              See it in action
             </Button>
           </Stack>
-          <Flex w={'full'}>
+          <Flex w={"full"}>
             {/* <Illustration height={{ sm: '24rem', lg: '28rem' }} mt={{ base: 12, sm: 16 }} /> */}
           </Flex>
         </Stack>
-      </Container >
-      <LoginModal isOpen={isOpenLoginModal} onOpen={onOpenLoginModal} onClose={onCloseLoginModal} fn={gotoDashboard} />
+      </Container>
+      <LoginModal
+        isOpen={isOpenLoginModal}
+        onOpen={onOpenLoginModal}
+        onClose={onCloseLoginModal}
+        fn={gotoDashboard}
+      />
     </>
-
-  )
+  );
 }
