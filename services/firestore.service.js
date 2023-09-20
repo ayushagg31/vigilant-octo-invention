06aa88daa0be32d67bbe716d585addf5888baff6
divya <dpj221@gmail.com>
2023-09-20 18:17:15 +0530

    add free tier missing plan

--- services/firestore.service.js
+++ services/firestore.service.js
@@ -9,7 +9,7 @@ import { app } from "../config/googleAuth.config";
 import { removeCollection } from "../config/qdrant.config";
 import logger from "./logging.service";
 import { deleteObject } from "./r2.service";
-import { plans } from "../config/plan.config";
+import { plans, FREE_TIER } from "../config/plan.config";
 
 const COLLECTION_LIMIT = 3;
 
