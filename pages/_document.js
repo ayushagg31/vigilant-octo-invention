bda38ec7b7c536e9cf2c040c32ca7b38df3938aa
divya <dpj221@gmail.com>
2023-09-20 20:20:26 +0530

    added title and icon

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ export default function Document() {
           ref="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css"
         />
+        <title>Doc Expert</title>
       </Head>
       <body style={{ background: '#ffffff' }}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
