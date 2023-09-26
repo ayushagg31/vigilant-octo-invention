21508fd4e3522f2f586f5e393da4ee6154552a43
Ayush <ayush@Ayushs-MacBook-Air.local>
2023-09-26 19:53:28 +0530

    hopefully this will fix ir

--- components/landing/Pricing.jsx
+++ components/landing/Pricing.jsx
@@ -15,7 +15,6 @@ import {
   useDisclosure,
   Flex,
 } from "@chakra-ui/react";
-import Head from "next/head";
 import { FaCheckCircle } from "react-icons/fa";
 import { pricingPlanConfig } from "../../config/pricing.plan";
 import { createCheckoutSessionApi } from "../../services/client.service";
@@ -159,9 +158,7 @@ export default function ThreeTierPricing() {
 
   return (
     <>
-      <Head>
-        <LemonLoader />
-      </Head>
+      <LemonLoader />
       <Flex
         h={"100vh"}
         align={"center"}
