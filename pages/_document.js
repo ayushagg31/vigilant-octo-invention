84bee409c041a32cc9c98992fb48a24ba6e3a826
Ayush <ayush@Ayushs-MacBook-Air.local>
2023-09-26 19:35:50 +0530

    fixed css

--- pages/_document.js
+++ pages/_document.js
@@ -7,12 +7,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link
-          global="true"
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
         />
         <link
-          global="true"
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css"
         />
