2c488ae3d175c49ed78ff1657248a9b5d83d1311
divya <dpj221@gmail.com>
2023-09-27 05:38:07 +0530

    updated prompt

--- scripts/ask-query.mjs
+++ scripts/ask-query.mjs
@@ -9,8 +9,7 @@ Follow Up Input: {question}
 Standalone question:`;
 
 const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+Please give a very descripted and detailed explanation of the question asked from the context provided of the document. Please provide structure answers. If the question is descriptive you can divided answer in sections , if required.
 
 {context}
 
