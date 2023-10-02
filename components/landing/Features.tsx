c6b7f3e9020290869d9a5b6c7500c983e4e457dd
Ayush <ayush@Ayushs-MacBook-Air.local>
2023-10-02 16:22:19 +0530

    dead code elimination

--- components/landing/Features.tsx
+++ components/landing/Features.tsx
@@ -2,42 +2,19 @@
 
 import {
   Image,
-  Flex,
   Heading,
   Text,
   Stack,
   Icon,
   Box,
 } from "@chakra-ui/react";
-import { ReactElement } from "react";
 import { FiUploadCloud } from "react-icons/fi";
 import { BsFillChatLeftTextFill } from "react-icons/bs";
 
-interface FeatureProps {
-  text: string;
-  iconBg: string;
-  icon?: ReactElement;
-}
 
-const Feature = ({ text, icon, iconBg }: FeatureProps) => {
-  return (
-    <Stack direction={"row"} align={"center"}>
-      <Flex
-        w={8}
-        h={8}
-        align={"center"}
-        justify={"center"}
-        rounded={"full"}
-        bg={iconBg}
-      >
-        {icon}
-      </Flex>
-      <Text fontWeight={600}>{text}</Text>
-    </Stack>
-  );
-};
 
 export default function SplitWithImage() {
+
   return (
     <Box
       id="demo-section"
