81da08d6e9d2c6ff56b5a0d4b9108748eb2925bb
CV Vikash <cvvkshcv@gmail.com>
2023-10-03 15:12:43 +0530

    removed persist

--- store/useCollections.js
+++ store/useCollections.js
@@ -8,48 +8,42 @@ import {
 import { FREE_TIER } from "../constants/plan.constants";
 
 export const useCollections = create(
-  persist(
-    (set, get) => ({
-      isLoading: true,
-      collections: [],
-      currentPlan: FREE_TIER,
-      usageInfo: {
-        pdf: {
-          count: 0,
-        },
-        mp3: {
-          count: 0,
-        },
-        query: {
-          count: 0,
-        },
+  (set, get) => ({
+    isLoading: true,
+    collections: [],
+    currentPlan: FREE_TIER,
+    usageInfo: {
+      pdf: {
+        count: 0,
       },
-      fetchCollections: async () => {
-        const { data } = await fetchCollectionsApi();
-        set({
-          collections: data?.collections || [],
-          currentPlan: data?.currentPlan,
-          usageInfo: data?.usageInfo || {},
-          isLoading: false,
-        });
+      mp3: {
+        count: 0,
       },
-      deleteCollection: async (collectionId) => {
-        try {
-          const { data } = await deleteCollectionApi({ collectionId });
-          set({ collections: data?.collections || [] });
-        } catch (error) {
-          throw new Error(error);
-        }
+      query: {
+        count: 0,
       },
-      updateUsageInfo: async () => {
-        try {
-          await updateUsageInfo();
-        } catch (error) {}
-      },
-    }),
-    {
-      name: "user-collections", // name of the item in the storage (must be unique)
-      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
-    }
-  )
+    },
+    fetchCollections: async () => {
+      const { data } = await fetchCollectionsApi();
+      set({
+        collections: data?.collections || [],
+        currentPlan: data?.currentPlan,
+        usageInfo: data?.usageInfo || {},
+        isLoading: false,
+      });
+    },
+    deleteCollection: async (collectionId) => {
+      try {
+        const { data } = await deleteCollectionApi({ collectionId });
+        set({ collections: data?.collections || [] });
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    updateUsageInfo: async () => {
+      try {
+        await updateUsageInfo();
+      } catch (error) {}
+    },
+  })
 );
